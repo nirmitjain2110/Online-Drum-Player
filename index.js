f3153cc6017836f58b8eabcd2dd68b5a696e5aba
Nirmit Jain <145483132+nirmitjain2110@users.noreply.github.com>
2024-09-30 15:47:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+for(let i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
+
+function handleClick(){
+    let buttonInnerHTML=this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+}
+}
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            let audio1= new Audio("sounds/crash.mp3");
+            audio1.play();
+            break;
+        case "a":
+            let audio2= new Audio("sounds/kick-bass.mp3");
+            audio2.play();
+            break;
+        case "s":
+            let audio3= new Audio("sounds/snare.mp3");
+            audio3.play();
+            break;
+        case "d":
+            let audio4= new Audio("sounds/tom-1.mp3");
+            audio4.play();
+            break;
+        case "j":
+            let audio5= new Audio("sounds/tom-2.mp3");
+            audio5.play();
+            break;
+        case "k":
+            let audio6= new Audio("sounds/tom-3.mp3");
+            audio6.play();
+            break;
+        case "l":
+            let audio7= new Audio("sounds/tom-4.mp3");
+            audio7.play();
+            break;
+    }
+}    
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+  
+    activeButton.classList.add("pressed");
+  
+    setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    }, 100);
+  
+  }
+  
